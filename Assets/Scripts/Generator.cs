dd749f76522ed17d3875383879b7ec1bb8e4dde1
Baraha <waterbulca@gmail.com>
2020-12-02 21:09:03 +0300

    StartProject

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public Transform player;
+    public Chunk[] Chunks;
+    private List<Chunk> spawnedChunks = new List<Chunk>();
+    public Chunk firstChunk;
+    
+    
+    void Start()
+    {
+        spawnedChunks.Add(firstChunk);
+    }
+    void Update()
+    {
+        if (player.position.x+15> spawnedChunks[spawnedChunks.Count - 1].endPoint.position.x)
+        {
+            CreateChunk();
+        }
+    }
+    private void CreateChunk()
+    {
+        Chunk createdchunk = Instantiate(Chunks[Random.Range(0, Chunks.Length)]);
+        createdchunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].endPoint.position - createdchunk.beginpoint.localPosition;
+        spawnedChunks.Add(createdchunk);
+        if (spawnedChunks.Count >= 5)
+        {
+            Destroy(spawnedChunks[0].gameObject);
+            spawnedChunks.RemoveAt(0);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float playerSpeed = 200f;
+    public float playerGetUp = 100f;
+    public Transform Camera;
+    public Transform player;
+    public Vector3 offset;
+    public Transform HeroPosition;
+    void FixedUpdate()
+    {
+        rb.transform.Translate(playerSpeed/10*Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(0,playerGetUp*Time.deltaTime,0);
+        }
+        Camera.transform.position = player.transform.position + offset;
+        HeroPosition.transform.position = player.transform.position;
+
+    }
+}
